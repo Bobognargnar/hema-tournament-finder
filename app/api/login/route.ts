9046b1dca1819952398545dcb17abf99e5bd3957
Fabrizio La Rosa <fabrizio.larosa@hotiday.it>
2025-07-15 11:34:54 +0200

    Update route.ts
    Co-Authored-By: Bobognargnar <114653007+Bobognargnar@users.noreply.github.com>

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,3 +1,4 @@
+import { Tournament } from "@/types/tournament"
 import { NextResponse } from "next/server"
 
 export async function POST(request: Request) {
@@ -5,30 +6,29 @@ export async function POST(request: Request) {
 
   if (email && password) {
     try {
-      const baseUrl = process.env.API_BASE_URL
-      const response = await fetch(`${baseUrl}/auth/v1/token?grant_type=password`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          apikey: process.env.NEXT_PUBLIC_API_KEY || "",
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-        }),
-      })
-
-      if (!response.ok) {
-        throw new Error(`API request failed: ${response.status} ${response.statusText}`)
+        const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL || process.env.API_BASE_URL
+        const response = await fetch(`${baseUrl}/auth/v1/token?grant_type=password`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'apikey': process.env.NEXT_PUBLIC_API_KEY || '',
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
+        })
+    
+        if (!response.ok) {
+          throw new Error(`API request failed: ${response.status} ${response.statusText}`)
+        }
+        const data = await response.json()
+        return NextResponse.json({ success: true, token: data.access_token,identity: data.user.email, message: "Login successful!" }, { status: 200 })
+    
+      } catch (error) {
+        return NextResponse.json({ success: false, message: "Login failed" }, { status: 400 })
       }
-      const data = await response.json()
-      return NextResponse.json(
-        { success: true, token: data.access_token, identity: data.user.email, message: "Login successful!" },
-        { status: 200 },
-      )
-    } catch (error) {
-      return NextResponse.json({ success: false, message: "Login failed" }, { status: 400 })
-    }
+
   } else {
     console.log("Login failed: Missing email or password")
     return NextResponse.json({ success: false, message: "Email and password are required." }, { status: 400 })
