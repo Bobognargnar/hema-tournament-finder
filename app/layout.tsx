ec18a9b4133d2f0411fdd15cc99663768dc9bf73
Fabrizio La Rosa <fabrizio.larosa@hotiday.it>
2025-12-13 17:01:07 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Hema Tournaments Finder',
+  description: 'Find HEMA tournaments anywhere in the world'
 }
 
 export default function RootLayout({
